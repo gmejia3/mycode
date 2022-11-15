0f961e5186f63a8636a07ebfac0edb364016a6b9
giovanni <gemejiajr@gmail.com>
2022-11-15 23:28:57 +0000

    About complex lists

---
+++ advlist/lab31.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import random
+
+wordbank = ["indentation", "spaces"]
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+print(wordbank)
+
+num = int(input("Choose a number between 0 and 20"))
+
+student_name =  tlgstudents[num]
+
+print(f"{student_name} always uses {wordbank[2]} {wordbank[1]} to indent.")
+
