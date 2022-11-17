5167cd5a75ebfe5e07cd82e56b4955f077abc3fe
giovanni <gemejiajr@gmail.com>
2022-11-17 19:00:41 +0000

    Working on if, elif, else logic

---
+++ iftest/conditional03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should we set for hostname?\n>")
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be MTG\nhostname matches expected config")
+
+print("This is the end of the script")    
+    
---
+++ iftest2/ipask.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+#This line now prompts the user for input
+ipchk = input("Apply an IP address: ")
+
+# a provided string will test true
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
+#If data is not provided.
+else:
+    print("You did not provie input.") #Indented
+    
