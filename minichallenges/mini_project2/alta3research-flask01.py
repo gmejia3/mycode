fce1740772ea2849116d7e5e9acc27d825fc2bd7
Giovanni <gemejiajr@gmail.com>
2022-11-28 18:54:48 +0000

    serving cookies

--- minichallenges/mini_project2/alta3research-flask01.py
+++ minichallenges/mini_project2/alta3research-flask01.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import render_template
 
 #Make an application called app with Flask
 
@@ -8,19 +12,13 @@ app = Flask(__name__)
 
 #Homepage of the game and goes to the index.html
 @app.route("/")
+@app.route("/start")
 def homepage():
     return render_template("index.html")
 
-
-
-
-
-
-
-
-
-
-
+@app.route("/options")
+def options():
+    return render_template("selections.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224) #Runs the application on local machine
---
+++ minichallenges/mini_project2/options.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+
+purchase_date = ("Day 1", "Day 2", "Day 3")
+
+purchase_results = { 
+                    "Congrats":
+                    [ "Alone": "Go buy some lottl tickets",
+                      "Group": "You better start paying off those favors",
+                      "Bots": "Yes, you are the problem its you"],
+                    "Sorry":
+                    ["Alone": "Sorry, the odds were against you, good luck on Ticketmaster!",
+                     "Group": "Hopefully you did not ask for too many favors",
+                     "Bots": "Yes, even though you failed, you are still the problem its you"]
+                    }
+
+
+
+def main():
+    print("I am working")
+
+
+
+if __name_ == "__main__":
+    main()
